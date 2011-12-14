57df365804bb83d4576c37200239e8667ec0d45a
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-12-14 15:18:40 +0100

    adding index.php

---
+++ www/index.php
@@ -0,0 +1,21 @@
+<style type="text/css" media="all">
+ @import url("/style.css");
+</style>
+
+<div id='header'><h1>CiviCRM books</h1></div>
+<div id='main'>
+<div id='menu'>
+<p id='edition'>Get involved</p>
+<p>You can help improve CiviCRM books and documentation by <a href="http://forum.civicrm.org/index.php/board,11.0.html">joining the documentation community</a>.</p>
+</p>
+</div>
+<div id='content'>
+This site is where you can find the official CiviCRM books developed and maintained by CiviCRM documentation community.<h2><a href="/user">User and administrator guide</a></h2>
+<p>a guide to the set up and every day use of your CiviCRM installation, covering planning, configuration, everyday use, reporting, etc.</p>
+<h2><a href="/developer">Developer guide</a></h2>
+<p>How to get started with CiviCRM development.  Suitable for those that want to extend and develop CiviCRM.</dd>
+</div>
+</div>
+<div id='footer'>
+	<p>CiviCRM documentation is community driven - <a href="http://forum.civicrm.org/index.php/board,11.0.html">join the documentation community!</a></p>
+</div>
