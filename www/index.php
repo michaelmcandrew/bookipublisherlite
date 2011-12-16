be6ba0450d24864e91ac51b5893548a74be30000
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-12-16 09:47:31 +0100

    improved front page content and linespacing when p tags are missing

--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@
 <div id='content'>
 This site contains the set of CiviCRM books developed and maintained by CiviCRM documentation community.
 <h2><a href="/user">User and administrator guide</a></h2>
-<p>A guide to the set up and every day use of your CiviCRM installation, covering planning, configuration, everyday use, reporting and more.</p>
+<p>A guide to the set up and every day use of CiviCRM, including planning, configuration, everyday use, reporting and more.</p>
 <h2><a href="/developer">Developer guide</a></h2>
 <p>How to get started with CiviCRM development.  Suitable for those that want to extend and develop CiviCRM.</p>
 </div>
