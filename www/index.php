3c4350101ba19f168ac130195fc5940d8f995d91
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-03-02 15:30:36 +0000

    adding breadcrumbs, title, and also coping with absolute URLS from objavi

--- www/index.php
+++ www/index.php
@@ -1,17 +1,23 @@
-<style type="text/css" media="all">
- @import url("/style.css");
-</style>
-<iframe scrolling="no" style="max-height: 35px; border: 0px; width: 100%; display: block;" src="http://civicrm.org/sites/civicrm.org/developers/index.html"></iframe>
-<div id='header'><h1>CiviCRM books</h1></div>
-<div id='main'>
-<div id='content'>
-This site contains the set of CiviCRM books developed and maintained by CiviCRM documentation community.
-<h2><a href="/user">User and administrator guide</a></h2>
-<p>A guide to the set up and every day use of CiviCRM, including planning, configuration, everyday use, reporting and more.</p>
-<h2><a href="/developer">Developer guide</a></h2>
-<p>How to get started with CiviCRM development.  Suitable for those that want to extend and develop CiviCRM.</p>
-</div>
-</div>
-<div id='footer'>
-	<p>CiviCRM documentation is community driven. You can <a href="http://forum.civicrm.org/index.php/board,11.0.html">help improve documentation.</a></p>
-</div>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" media="all" href="/style.css" />
+</head>
+
+<body>
+	<iframe scrolling="no" style="max-height: 35px; border: 0px; width: 100%; display: block;" src="http://civicrm.org/sites/civicrm.org/developers/index.html"></iframe>
+	<div id='header'><h1>CiviCRM books</h1></div>
+	<div id='main'>
+	<div id='content'>
+	This site contains the set of CiviCRM books developed and maintained by CiviCRM documentation community.
+	<h2><a href="/user">User and administrator guide</a></h2>
+	<p>A guide to the set up and every day use of CiviCRM, including planning, configuration, everyday use, reporting and more.</p>
+	<h2><a href="/developer">Developer guide</a></h2>
+	<p>How to get started with CiviCRM development.  Suitable for those that want to extend and develop CiviCRM.</p>
+	</div>
+	</div>
+	<div id='footer'>
+		<p>CiviCRM documentation is community driven. You can <a href="http://civicrm.org/documentation">help improve documentation.</a></p>
+	</div>
+</body>
+
+</html>
