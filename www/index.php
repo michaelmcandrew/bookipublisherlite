20bc2d346431d6de51c9fea6552ad8b039be1d5c
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-09-26 21:55:30 +0200

    book home page working

--- www/index.php
+++ www/index.php
@@ -14,11 +14,17 @@ $menu .= '</ul></div>';
 
 
 $replacements = array(
-    'title' => "CiviCRM book search",
+    'title' => "CiviCRM books",
     'header' => "CiviCRM books",
-    'menu' => $menu,
-    'footer' => 'CiviCRM documentation is community driven. You can <a href="http://civicrm.org/participate/documentation">help improve documentation.</a>',
-    'content' => '<h1>Search books</h1><gcse:searchbox-only></gcse:searchbox-only><gcse:searchresults-only></gcse:searchresults-only>'
+    'menu' => '',
+    'content' => '
+	<p>This site contains the set of CiviCRM books developed and maintained by CiviCRM community.<p>
+	<h2><a href="/user">User and administrator guide</a></h2>
+	<p>A guide to the set up and every day use of CiviCRM, including planning, configuration, everyday use, reporting and more.</p>
+	<h2><a href="/developer">Developer guide</a></h2>
+	<p>How to get started with CiviCRM development.  Suitable for those that want to extend and develop CiviCRM.</p>
+	',
+    'footer' => 'CiviCRM documentation is community driven. You can <a href="http://civicrm.org/participate/documentation">help improve documentation.</a>'
     
 );
 
