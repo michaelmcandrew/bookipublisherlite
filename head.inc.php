8c4a9ec48c71a10b5aee008e6701c9192cf9b4a7
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-09-28 23:24:45 +0200

    oops - the jquery was a bit weird

---
+++ head.inc.php
@@ -0,0 +1,3 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="http://balupton.github.com/jquery-syntaxhighlighter/scripts/jquery.syntaxhighlighter.min.js"></script>
+<script type="text/javascript">$.SyntaxHighlighter.init();</script>
