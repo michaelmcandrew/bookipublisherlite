f12b9a867d48c047acf02fc07778ddbb1e7b6bde
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-09-29 00:06:27 +0200

    jquery syntax highlighting nirvana

--- head.inc.php
+++ head.inc.php
@@ -1,3 +1,8 @@
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 <script type="text/javascript" src="http://balupton.github.com/jquery-syntaxhighlighter/scripts/jquery.syntaxhighlighter.min.js"></script>
-<script type="text/javascript">$.SyntaxHighlighter.init();</script>
+<script type="text/javascript">
+$(function(){
+    $('pre').addClass('language-php');
+    $.SyntaxHighlighter.init();	    
+});
+</script>
