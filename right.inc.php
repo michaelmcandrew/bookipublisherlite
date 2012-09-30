550251e6a350c279a24f316cb902d17af645f646
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-09-30 11:06:42 +0000

    adding prominent get involved and flossmanuals links

--- right.inc.php
+++ right.inc.php
@@ -12,4 +12,8 @@ foreach($types as $type){
 <div id="pdf"><a href="<?php echo $file['pdf']; ?>"><img src="/download.png"> download as pdf</a></div>
 <div id="epub"><a href="<?php echo $file['epub']; ?>"><img src="/download.png"> download as ePub</a></div>
 </div>
-
+<hr />
+<h2>Get involved</h2>
+<p>CiviCRM documentation is community driven. You can <a href="http://civicrm.org/participate/documentation">help improve documentation</a>.</p>
+<hr />
+<p>Powered by <a href="http://www.flossmanuals.org">Flossmanuals</a>.</p>
